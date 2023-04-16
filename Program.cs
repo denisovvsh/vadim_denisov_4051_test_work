533fb99154553d6969c3da0b6ce6dfb46087b054
Vadim Denisov <vadim.denisov@advcake.com>
2023-04-16 09:05:01 +0700

    Реализовано получение исходного массива от пользователя

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+
+ */
+
+Console.Clear();
+
+string[] arrayStrings = CreateArrayStrings();
+
+PrintArray(arrayStrings);
+
+string[] CreateArrayStrings()
+{
+    Console.Write("Введите элементы массива, через запятую: ");
+    string[] arr = Console.ReadLine().Split(',');
+    return arr; 
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
